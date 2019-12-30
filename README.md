50a4b34384194b62dc61ef3416d04ef2563d9537
François Mentec <francois.mentec@alten.com>
2019-12-30 12:00:29 +0100

    Link input moved inside the page, refactoring

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PoE-Trade-Extension
-A chrome extension to check the impact an item on https://www.pathofexile.com/trade has on your build.
+A chrome extension to check the impact an item on https://www.pathofexile.com/trade has on your build.  
+
+Technically the extension works with firefox, but pob.party doesn't (you can't paste text).
 
 ![](img/capture-2.png?raw=true)
 
@@ -11,16 +13,17 @@ A chrome extension to check the impact an item on https://www.pathofexile.com/tr
 - Select the **PoE-Trade-Extension** folder you just downloaded
 
 ## Use
-- Import your build in PoB (not gonna explain how to do that)
+- Import your build in PoB (the desktop version)
 - Be sure to select the spell on which you want to see the impact
 - Export your build, copy the code
 - Import the build on https://pob.party/
 - Get a sharable link using the **Share & Config button** (bottom left)
-- You should see a new icon in the top right corner of your browser, click on it, paste the link then click on **SET POB** ![](img/popup.png?raw=true)
-- Now you can go on https://www.pathofexile.com/trade (doesn't work on poe.trade) and check the impact an item has on your build
+- You should see a icon in the top left corner of https://www.pathofexile.com/trade page, click on it, paste the link in the text field then click on **SET LINK** ![](img/control-panel.png?raw=true)
+- Now you can search items and check the impact an item has on your build
 
 ## Why it may not work
-- Your browser window is too small, so the iframe doesn't show all the required data
+- Your browser window is too small, so the iframe doesn't show all the required data.
+- It conflict with another script/extension. I can fix this if you fill an issue and provide me with enough information.
 
 ## How does it work
 It opens pob.party in an iframe then interact with it using user inputs and hooking drawing functions. It's not very clean but it works.
--- js/trade.js
+++ js/trade.js
@@ -1,14 +1,8 @@
+let storage = chrome.storage.sync
 let pob = null
-let pobLink = null
-
-function loadPob () { // Create pob iframe
-  if (pobLink == null) throw new Error('PoB link is null')
-
-  pob = document.createElement('iframe')
-  pob.setAttribute('id', 'pob-iframe')
-  pob.setAttribute('src', pobLink)
-  document.body.appendChild(pob)
-}
+let script = null
+let pobRegex = /^https:\/\/pob.party\/share\/[a-z]*$/
+let icon = `<img src="${chrome.extension.getURL('img/icon-40.png')}">`
 
 // Build User Interface
 let controlPanel = document.createElement('div')
@@ -18,14 +12,20 @@ document.body.appendChild(controlPanel)
 let togglePanelButton = document.createElement('button')
 togglePanelButton.setAttribute('id', 'toggle-panel-button')
 togglePanelButton.className = 'pte-button'
-togglePanelButton.innerHTML = 'Show'
+//togglePanelButton.innerHTML = 'Show'
+togglePanelButton.innerHTML = icon
 togglePanelButton.addEventListener('click', e => {
   controlPanel.classList.toggle('visible')
   togglePanelButton.innerHTML = controlPanel.classList.contains('visible')
-    ? 'Hide'
-    : 'Show'
+    ? '&times;'
+    : icon
 })
 document.body.appendChild(togglePanelButton)
+
+let panelTitle = document.createElement('div')
+panelTitle.setAttribute('id', 'panel-title')
+panelTitle.innerHTML = 'PoE Trade Extension'
+controlPanel.appendChild(panelTitle)
 
 let toggleButton = document.createElement('button')
 toggleButton.className = 'pte-button'
@@ -57,6 +57,45 @@ controlPanel.appendChild(togglePobVisibleButton)
 
 controlPanel.appendChild(document.createElement('br'))
 
+// PoB link input
+let pobLinkInput = document.createElement('input')
+pobLinkInput.className = 'pte-input'
+pobLinkInput.setAttribute('placeholder', 'pob.party sharing link')
+controlPanel.appendChild(pobLinkInput)
+
+let pobLinkButton = document.createElement('button')
+pobLinkButton.className = 'pte-button'
+pobLinkButton.innerHTML = 'SET LINK'
+pobLinkButton.addEventListener('click', e => {
+  storage.set({build_code: pobLinkInput.value}, () => {
+    message('Build link set: ' + pobLinkInput.value, 'message', 5000)
+    loadPob()
+  })
+})
+controlPanel.appendChild(pobLinkButton)
+
+// Message
+let messageDiv = document.createElement('div')
+messageDiv.setAttribute('id', 'pte-message')
+controlPanel.appendChild(messageDiv)
+
+function message (content, type='message', timeout=null) {
+  messageDiv.className = type
+  messageDiv.innerHTML = content
+  if (type == 'error') {
+    console.error(content)
+  } else {
+    console.log(content)
+  }
+
+  if (timeout != null) {
+    setTimeout(() => {
+      messageDiv.className = ''
+      messageDiv.innerHTML = ''
+    }, timeout)
+  }
+}
+
 let githubLogo = `<svg class="octicon octicon-mark-github v-align-middle" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" fill="white" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`
 
 let githubLink = document.createElement('a')
@@ -67,21 +106,33 @@ githubLink.innerHTML = githubLogo + 'GitHub'
 controlPanel.appendChild(githubLink)
 
 // Get pob link, load pob and inject code
-chrome.storage.sync.get(['build_code'], res => {
-  if (typeof res.build_code == 'string') {
-    if (res.build_code.match(/^https:\/\/pob.party\/share\/[a-z]*$/) != null) {
-      pobLink = res.build_code
-      loadPob()
+function loadPob () { // Create pob iframe
+  storage.get(['build_code'], res => {
+    if (typeof res.build_code == 'string') {
+      if (res.build_code.match(/^https:\/\/pob.party\/share\/[a-z]*$/) != null) {
+        pobLinkInput.value = res.build_code
 
-      // Inject code
-      let script = document.createElement('script')
-      script.setAttribute('type', 'text/javascript')
-      script.setAttribute('src', chrome.extension.getURL('js/trade-injected.js'))
-      document.body.appendChild(script)
+        pob = document.createElement('iframe')
+        pob.setAttribute('id', 'pob-iframe')
+        pob.setAttribute('src', res.build_code)
+        document.body.appendChild(pob)
+
+        injectCode()
+      } else {
+        message('Build link is incorrect: ' + res.build_code, 'error')
+      }
     } else {
-      console.error('Build link is incorrect: ' + res.build_code)
+      message('pob.party link is not defined', 'error')
     }
-  } else {
-    console.error('No build link not defined')
-  }
-})
+  })
+}
+
+function injectCode () {
+  if (script != null) return
+  script = document.createElement('script')
+  script.setAttribute('type', 'text/javascript')
+  script.setAttribute('src', chrome.extension.getURL('js/trade-injected.js'))
+  document.body.appendChild(script)
+}
+
+loadPob()
